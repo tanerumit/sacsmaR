89851561f2ea2957a76bd638dff6ab47b0026f76
tanerumit <tanerumit@gmail.com>
2017-10-12 11:19:31 -0400

    Include multi-outlet channel routing

--- R/sacHydroSim.R
+++ R/sacHydroSim.R
@@ -18,54 +18,84 @@
 #' @rdname sacHydroSim
 #' @export 
 sacHydroSim <- function(par_petHamon, par_snow17, par_sacsma, par_routLah,
-                        tavg, prcp, lat, elev, area, flowLength, 
-                        dayOfYear, lastDayOfYear) {
+                        hru_tavg, hru_prcp, hru_lat, hru_elev, hru_area, 
+                        hru_flowLength, hru_subcat = NULL, dayOfYear, 
+                        lastDayOfYear) {
   
-  sim_num <- length(prcp[[1]])
-  hru_num <- length(tavg)
-
-  ### CALCULATE PET FOR ALL HRUs
-  pet <- lapply(1:hru_num, function(x) 
-    petHamon(coeff = par_petHamon[x], tavg = tavg[[x]], lat = lat[[x]], 
-             dayOfYear = dayOfYear))
-  
-  ### CALCULATE SNOW MODULE FOR ALL HRUs
-  snowMelt <- lapply(1:hru_num, function(x) 
-    snow17(par = par_snow17[x,], 
-           prcp = prcp[[x]], tavg = tavg[[x]], elev = elev[[x]],
-           dayOfYear = dayOfYear, lastDayOfYear =  lastDayOfYear)) 
-  
-  ### CALCULATE STREAMFLOW GENERATION AT ALL HRUs
-  hru_simflow <- lapply(1:hru_num, function(x) 
-    sacSma(par = par_sacsma[x,], 
-           prcp = snowMelt[[x]], 
-           pet = pet[[x]], 
-           lat = lat[x], 
-           elev = elev[x]))
-  
-  ### Fractional flow
-  hru_simflow <- lapply(1:hru_num, function(x)
-                        hru_simflow[[x]] * area[x] / sum(area))
-  
-  ### CALCULATE ROUTING
-  UH_river <- lapply(1:hru_num, function(x) 
-    routeLohamann(par = par_routLah[x,], flowLength = flowLength[x]))
-  
-  ### CONVOLUTION
   # Base Time of Unit Hydrographs of HRU and River in Routing model. 
   KE      <-  12  # Base time for HRU UH (day)
   UH_DAY  <-  96  # Base time for river routing UH (day)
   
-  FLOW <- vector("numeric", length = sim_num)
+  sim_num <- length(hru_prcp[[1]])
+  hru_num <- length(hru_tavg)
+
+  ### CALCULATE PET FOR ALL HRUs
+  hru_pet <- lapply(1:hru_num, function(x) 
+    petHamon(coeff = par_petHamon[x], tavg = hru_tavg[[x]], lat = hru_lat[[x]], 
+             dayOfYear = dayOfYear))
+  
+  ### CALCULATE SNOW MODULE FOR ALL HRUs
+  hru_meltNrain <- lapply(1:hru_num, function(x) 
+    snow17(par = par_snow17[x,], prcp = hru_prcp[[x]], tavg = hru_tavg[[x]], 
+           elev = hru_elev[[x]], dayOfYear = dayOfYear, 
+           lastDayOfYear =  lastDayOfYear)) 
+  
+  ### CALCULATE STREAMFLOW GENERATION AT ALL HRUs
+  hru_simflow <- lapply(1:hru_num, function(x) 
+    sacSma(par = par_sacsma[x,], prcp = hru_meltNrain[[x]], pet = hru_pet[[x]], 
+           lat = hru_lat[x], elev = hru_elev[x]))
+  hru_simflow <- lapply(1:hru_num, function(x)
+                        hru_simflow[[x]] * area[x] / sum(area))
+  ### CHANNEL ROUTING
+  UH_river <- lapply(1:hru_num, function(x) 
+    routeLohamann(par = par_routLah[x,], flowLength = hru_flowLength[x]))
+  
+  ### CONVOLUTION
   j_i <- 1:(KE+UH_DAY-1) 
   j <- lapply(1:sim_num, function(i) j_i[i-j_i+1 >= 1])
+  
+  #convolution for a single outlet 
+  if (is.null(hru_subat)) {
 
-  for (n in 1:hru_num) {
-    for(i in 1:sim_num) {
-      FLOW[i] <- FLOW[i] + sum(UH_river[[n]][j[[i]]] * hru_simflow[[n]][i-j[[i]]+1])
+    #Vector to store the final flow computed
+    FLOW <- vector("numeric", length = sim_num)
+    
+    for (n in 1:hru_num) {
+      for(i in 1:sim_num) {
+        FLOW[i] <- FLOW[i] + sum(UH_river[[n]][j[[i]]] * hru_simflow[[n]][i-j[[i]]+1])
+      }
     }
-  }
+
+  #convolution for multiple outlets  
+  } else {
+    
+    #number of subcatchments
+    subc_num <- length(unique(hru_subcat))
+    
+    #Vector to store the final flow computed
+    FLOW <- rep(list(vector(mode = "numeric", length = sim_num)), subc_num)
+    
+    #Repeat for all outlet locations
+    for (k in 1:subc_num) {
+      
+      #indices of hrus in the subcat
+      hru_ind <- which(hru_subcat == k)
+      
+      #loop through each subcat
+      for (n in 1:length(hru_ind)) {
+        
+        FLOW <- vector("numeric", length = sim_num)
+        for (n in 1:hru_num) {
+          for(i in 1:sim_num) {
+            FLOW[[k]][i] <- FLOW[[k]][i] + sum(UH_river[[n]][j[[i]]] * hru_simflow[[n]][i-j[[i]]+1])
+          }
+        }
+      }
+    }
+  } 
   
   return(FLOW)
-
-}
+}  
+  
+  
+# Current progress: work on the multi-outlet channel routing part
